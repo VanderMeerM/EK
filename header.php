ef49f1d5bcfd1227d0c3141ad3622a6c0dc73a1e
Marcel van der Meer <marcelmeer@gmail.com>
2024-07-06 19:32:39 +0200

    removed former buttons

--- header.php
+++ header.php
@@ -11,21 +11,7 @@ strtoupper($todaysmatches) . '
 
 <div class= "btn_container"> ' .
 
-/*
-<a href="' . $_SERVER['PHP_SELF'] . '?date=' . date('Y-m-d', strtotime('yesterday')) . '" 
-' . (date('Y-m-d', strtotime('yesterday')) === $_GET['date'] ?  'class="txt-underline"' : null). '> 
-'. $yesterday . '</a> 
-
-<a href="' . $_SERVER['PHP_SELF'] . '"
-' . ((date('Y-m-d') === $_GET['date'] || !$_GET['date']) ?  'class="txt-underline"' : null). '> 
-'. $today . '</a> 
-
-<a href="' . $_SERVER['PHP_SELF'] . '?date=' . date('Y-m-d', strtotime('tomorrow')) . '"
-' . (date('Y-m-d', strtotime('tomorrow')) === $_GET['date'] ?  'class="txt-underline"' : null). '>
-'. $tomorrow . '</a>
-*/
-'
-<div>
+'<div>
   <input type="text" id="datepicker" value = ' . (!$_GET['date'] ? date('Y-m-d') : $_GET['date']) . '>
 </div>
 </div> 
